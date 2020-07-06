5062dddcc0929ed1b3544768d2e9b6c66bf84a30
morgan <morgan_kelly.wixted@dfki.de>
2020-07-06 23:43:08 +0200

    added potential fix to save feature vectors as tensors

--- source/setup.py
+++ source/setup.py
@@ -6,6 +6,8 @@ from Constants import asjp_char_dims, asjp_char_as_vector
 from classes.Characters import ASJPChar
 from classes.Alphabets import ASJPAlphabet
 from classes.Words import Word
+import tensorflow as tf
+import numpy as np
 
 def check_vector_dims():
 
@@ -27,7 +29,13 @@ def test_ASJP_alphabet():
     translated = asjp.translate(word)
     print(translated)
     array = translated.get_feature_array()
-    print(array)
+    #print(array)
+    '''Added by Morgan'''
+    #this will save a single tensor
+    #save the array to a file
+    np.save("test", array, allow_pickle=False)
+    #open the file to make sure everything worked
+    print(np.load("test.npy"))
 
 
 def main():
