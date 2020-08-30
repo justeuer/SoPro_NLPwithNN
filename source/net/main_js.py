2177f7af9862241aca72a78a6c8d43f871fcf698
justeuer <julius.steuer@mailbox.org>
2020-08-30 15:47:10 +0200

    new main script

--- source/net/main_js.py
+++ source/net/main_js.py
@@ -2,20 +2,35 @@ from argparse import ArgumentParser
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
+import time
+import sys
 
-from classes import Alphabet, CognateSet
-from net.model import Encoder
+sys.path.insert(0, "/home/julius/git/my_repos/sopro-nlpwithnn/")
+
+from source.classes import Alphabet, CognateSet
+from source.net.model import Encoder, Decoder
+from source.net.train import loss_function, train_step
 
 HEADER_ROW = 0
 COLUMN_SEPARATOR = ","
 ID_COLUMN = 0
 CONCEPT_COLUMN = 1
+BATCH_SIZE = 1
 
+embedding_dim = 10
+vocab_input_size = 26
+
+encoder = Encoder(vocab_input_size, embedding_dim, encoded_units=10, batch_size=BATCH_SIZE)
+decoder = Decoder(vocab_input_size, embedding_dim, decoded_units=10, batch_size=BATCH_SIZE)
+
+optimizer = tf.keras.optimizers.Adam()
+#loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+#loss_object = tf.keras.losses.CosineSimilarity()
 models = ['asjp', 'ipa', 'latin']
 
 valid_size = 0.8
 batch_size = 1
-pad_to = 6 # also vocab size
+pad_to = 6  # also vocab size
 
 
 def parser_args():
@@ -68,7 +83,7 @@ def main():
         row_split = line.split(COLUMN_SEPARATOR)
         id = row_split[ID_COLUMN]
         concept = row_split[CONCEPT_COLUMN]
-        words = row_split[CONCEPT_COLUMN+1:]
+        words = row_split[CONCEPT_COLUMN + 1:]
         cognate_dict = {}
         assert len(langs) == len(words), "Langs / Words mismatch, expected {}, got {}".format(len(langs), len(words))
         for lang, word in zip(langs, words):
@@ -77,32 +92,37 @@ def main():
                         pad_to=pad_to)
         cognate_sets.append(cs)
 
-    split_index = int(valid_size*len(cognate_sets))
+    split_index = int(valid_size * len(cognate_sets))
     train_data = cognate_sets[:split_index]
     valid_data = cognate_sets[split_index:]
     print("train size: {}".format(len(train_data)))
     print("valid size: {}".format(len(valid_data)))
 
     for epoch in range(epochs):
-        for cs in train_data:
+        epoch_start = time.time()
+        encoded_hidden = encoder.initialize_hidden_state()
+        total_loss = 0
+
+        for i, cs in enumerate(train_data):
             print(cs)
-            for char_embedding in cs:
-                #print(char_embedding)
+            for j, char_embedding in enumerate(cs):
+                # print(char_embedding)
                 target = char_embedding.pop(cs.ancestor)
                 data = char_embedding
-                #print(target)
-                #print()
-                #print()
-                #print(data)
                 print("target (latin)", target.to_numpy())
                 for lang, embedding in data.items():
                     print("embedding ({})".format(lang), embedding.to_numpy())
-                    # TODO: here the actual training should happen
-            break
-        break
+                    batch_loss = train_step(embedding.to_numpy(), target, encoder, decoder, encoded_hidden)
+                    total_loss += batch_loss
+                if i % 100:
+                    print('Epoch {} Batch {} Loss {:.4f}'.format(epoch + 1,
+                                                                     i,
+                                                                     batch_loss.numpy()))
+                #print('Epoch {} Loss {:.4f}'.format(epoch + 1,
+                #                                            total_loss / steps_per_epoch))
+                print('Time taken for 1 epoch {} sec\n'.format(time.time() - epoch_start))
 
         # TODO: evaluate
-
 
 
 if __name__ == '__main__':
